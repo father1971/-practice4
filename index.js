b1f9bdb019fee5b223908865831a138b3d843b0b
father1971 <rotanovav@gmail.com>
2023-12-23 13:55:01 +0300

    total expences

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-let sum = 2 + 3;
-console.log(sum);
+let number_of_employees = 10;
+let salary = 2000;
+let total_salary =
+number_of_employees * salary;
+console.log(total_salary);
+let other_expences = 10000;
+let total_expences =
+total_salary + other_expences;
+console.log(total_expences);
